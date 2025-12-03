076336c28cdc7cce80db7f028ab4b74a0bd3a348
Caroline Forslund <hello@codingcaroline.com>
2025-12-03 09:07:50 +0100

    1.7: unicafe step 2

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [ good, setGood ] = useState(0)
+  const [ neutral, setNeutral ] = useState(0)
+  const [ bad, setBad ] = useState(0)
+  const [ all, setAll ] = useState(0)
+  const [ allVotes, setAllVotes ] = useState([])
 
-  const increaseGood = () => { setGood( good + 1 ) }
-  const increaseNeutral = () => { setNeutral( neutral + 1 ) }
-  const increaseBad = () => { setBad( bad + 1 ) }
+  const increaseGood = () => {
+    setGood( good + 1 )
+    setAll( all + 1 )
+    setAllVotes( allVotes.concat( 1 ) )
+  }
+
+  const increaseNeutral = () => {
+    setNeutral( neutral + 1 )
+    setAll( all + 1 )
+    setAllVotes( allVotes.concat( 0 ) )
+  }
+  const increaseBad = () => { 
+    setBad( bad + 1 )
+    setAll( all + 1 )
+    setAllVotes( allVotes.concat( -1 ) )
+  }
 
   return (
     <div>
@@ -18,7 +32,7 @@ const App = () => {
       <Button text="Bad" onClick={increaseBad} />
 
       <Heading type="h2" text="Statistics" />
-      <Display good={good} neutral={neutral} bad={bad} />
+      <Display good={good} neutral={neutral} bad={bad} all={all} allVotes={allVotes} />
     </div>
   )
 }
@@ -32,12 +46,21 @@ const Heading = ({type, text}) => {
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
-const Display = ({good, neutral, bad}) => {
+const Display = ({good, neutral, bad, all, allVotes}) => {
+  const average = allVotes.length === 0
+    ? 0 
+    : allVotes.reduce((sum, vote) => sum + vote, 0) / allVotes.length
+  const positive = all === 0
+    ? 0
+    : good / all * 100
   return (
     <p>
       Good {good} <br />
       Neutral {neutral} <br />
-      Bad {bad}
+      Bad {bad} <br />
+      All {all} <br />
+      Average {average} <br />
+      Positive {positive}%
     </p>
   )
 }
