4733c7f2dda276da6407d389514e74cdd2301dbc
Caroline Forslund <hello@codingcaroline.com>
2025-12-04 19:39:06 +0100

    2.8: The Phonebook Step 3

--- part2/phonebook/phonebook/src/App.jsx
+++ part2/phonebook/phonebook/src/App.jsx
@@ -6,25 +6,36 @@ import Person from './components/Person'
 const App = () => {
   const [idCount, setIdCount] = useState(1)
   const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas' }
+    { id: 1, name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const handleChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
 
   const addPerson = (event) => {
     event.preventDefault()
 
-    if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
-      return
+    if ( persons.some(person => person.name === newName ) || persons.some(person => person.number === newNumber) ) {
+      if ( persons.some(person => person.name === newName ) ) {
+        alert(`${newName} is already added to phonebook`)
+      }
+      if ( persons.some(person => person.number === newNumber ) ) {
+        alert(`${newNumber} is already added to phonebook`)
+      }
     }
     else {
       const newPerson = {
         id: idCount + 1,
-        name: newName
+        name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(newPerson))
       setIdCount(idCount + 1)
@@ -40,7 +51,16 @@ const App = () => {
           name: 
           <input 
             value={newName}
-            onChange={handleChange}
+            onChange={handleNameChange}
+            required
+          />
+        </div>
+         <div>
+          number: 
+          <input 
+            value={newNumber}
+            onChange={handleNumberChange}
+            required
           />
         </div>
         <div>
--- part2/phonebook/phonebook/src/components/Person.jsx
+++ part2/phonebook/phonebook/src/components/Person.jsx
@@ -1,6 +1,6 @@
 const Person = ({person}) => {
 
-    return <li key={person.id}>{person.name}</li>
+    return <li key={person.id}>{person.name} {person.number}</li>
 }
 
 export default Person
