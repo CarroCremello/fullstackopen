c57d184a8aa7cd6453b8f57ddffa0e85b360714f
Caroline Forslund <hello@codingcaroline.com>
2026-01-12 15:27:29 +0200

    3.11 Full Stack Phonebook (base url update)

--- part3/phonebook_fontend/src/services/persons.js
+++ part3/phonebook_fontend/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 let baseUrl
-import.meta.env.DEV ? baseUrl = 'http://localhost:3001/api/persons' : baseUrl = '/api/persons'
+import.meta.env.DEV ? baseUrl = 'http://localhost:3001/api/persons' : baseUrl = import.meta.env.BASE_URL
 
 console.log("baseUrl:", baseUrl);  
 
