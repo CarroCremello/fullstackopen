f42e3483e7df35b0d221bf80ce48d7a2e5d9fd16
Caroline Forslund <hello@codingcaroline.com>
2026-01-13 18:46:32 +0200

    3.18*: Phonebook database step 6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -6,28 +6,7 @@ const cors = require('cors')
 const Person = require('./models/person')
 const app = express()
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+let persons = []
 
 morgan.token('body', (request) => {  
   if (request.method !== 'POST') {
@@ -49,9 +28,10 @@ app.get('/', (request, response) => {
 
 app.get('/info', (request, response) => {
     const date = new Date()
+    const numberOfContacts = Person.count()
     response.send(
         `<h1>Info</h1>
-        <p>Your phonebook has ${persons.length} contacts</p>
+        <p>Your phonebook has ${numberOfContacts} contacts</p>
         <p>${date}</p>`
     )
 })
