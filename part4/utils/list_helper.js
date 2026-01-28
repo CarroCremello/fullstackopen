5136d9757ad983652a3284a3ee086e5eb3353f4b
Caroline Forslund <hello@codingcaroline.com>
2026-01-28 15:02:46 +0200

    4.6*: Helper Functions and Unit Tests, step 4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,12 +1,25 @@
+const _ = require('lodash')
+
 const totalLikes = (blogs) => {
-    return blogs.reduce((accumulator, blog) => accumulator + blog.likes, 0);    
+    return blogs.reduce((accumulator, blog) => accumulator + blog.likes, 0)
 }
 
 const favoriteBlog = (blogs) => {
     return blogs.reduce((previous, current) => previous.likes > current.likes ? previous : current)
 }
 
+const mostBlogs = (blogs) => {
+    const countedBlogs = _.countBy(blogs, 'author')
+    const [author, authorBlogs] = _.maxBy(Object.entries(countedBlogs),([author, count]) => count)
+
+    return {
+        author,
+        blogs: authorBlogs,
+    }
+}
+
 module.exports = {
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
