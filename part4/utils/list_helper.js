e688f2b7476d2eda1b808de760e265f3fd71e3e8
Caroline Forslund <hello@codingcaroline.com>
2026-01-28 14:26:38 +0200

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,11 @@ const totalLikes = (blogs) => {
     return blogs.reduce((accumulator, blog) => accumulator + blog.likes, 0);    
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((previous, current) => previous.likes > current.likes ? previous : current)
+}
+
 module.exports = {
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
