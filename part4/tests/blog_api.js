60aa24e83b5caaf6be144322cddae52a1ea73da3
Caroline Forslund <hello@codingcaroline.com>
2026-03-01 17:53:05 +0100

    4.9: Blog List Tests, step 2

--- part4/tests/blog_api.js
+++ part4/tests/blog_api.js
@@ -1,13 +1,35 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: "Hello World!",
+    author: "John Doe",
+    url: "https://fullstackopen.com/en"
+  },
+  {
+    title: "Beautiful World",
+    author: "Jane Doe",
+    url: "https://fullstackopen.com/en"
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
 
 test('all blog posts are returned', async () => {
   const response = await api.get('/api/blogs')
-  assert.strictEqual(response.body.length, 1)
+  assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
 test('blogs are returned as json', async () => {
@@ -15,6 +37,13 @@ test('blogs are returned as json', async () => {
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('unique identifier property of blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+  const blog = response.body[0]
+  assert.ok(blog.id)
+  assert.strictEqual(blog._id, undefined)
 })
 
 after(async () => {
