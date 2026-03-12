35f7ea9942174448136b53b54c4d055b9692ec9f
Caroline Forslund <hello@codingcaroline.com>
2026-03-12 12:30:10 +0100

    4.18: Blog List Expansion, step 6

--- part4/app.js
+++ part4/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 const app = express()
@@ -8,6 +9,7 @@ const app = express()
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler) // this has to be the last loaded middleware, also all the routes should be registered before this!
