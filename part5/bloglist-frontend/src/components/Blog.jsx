6739dc48c9f2ae4862e667a362bf2c975af918e3
Caroline Forslund <hello@codingcaroline.com>
2026-03-21 17:07:58 +0100

    5.7 Blog List Frontend, step 7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  const [open, setOpen] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      {!open && <button onClick={() => setOpen(true)}>View</button>}
+      {open && (
+        <>
+          <button onClick={() => setOpen(false)}>Close</button>
+          <br />
+          {blog.url}
+          <br />
+          Likes : {blog.likes}
+          <button>Like</button>
+          <br />
+          {blog.user.name}
+        </>
+      )}
+
+
+    </div>  
+  )
+}
+
+  
+
 
 export default Blog
