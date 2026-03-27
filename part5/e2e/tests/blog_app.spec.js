acf153137ddc5d53b8eb5feaaf759d80ecbeca9f
Caroline Forslund <hello@codingcaroline.com>
2026-03-27 11:43:38 +0100

    5.18: Blog List End To End Testing, step 2

--- part5/e2e/tests/blog_app.spec.js
+++ part5/e2e/tests/blog_app.spec.js
@@ -1,12 +1,34 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: { username: 'testuser', name: 'Test User', password: 'testpassword' }
+    })
     await page.goto('http://localhost:5173')
   })
 
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByRole('textbox', { name: 'username' })).toBeVisible()
     await expect(page.locator('input[type="password"]')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByRole('textbox', { name: 'username' }).fill('testuser')
+      await page.locator('input[type="password"]').fill('testpassword')
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(page.getByText('Test User is logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByRole('textbox', { name: 'username' }).fill('testuser')
+      await page.locator('input[type="password"]').fill('wrongpassword')
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(page.getByText('Wrong username or password')).toBeVisible()
+    })
   })
 })
