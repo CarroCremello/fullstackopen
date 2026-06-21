056b4777c578792d017f27f403841959cb9228a4
Caroline Forslund <hello@codingcaroline.com>
2026-06-21 12:00:08 +0200

    6.8 anecdotes, step7

--- part6/anecdotes/src/components/AnecdoteForm.jsx
+++ part6/anecdotes/src/components/AnecdoteForm.jsx
@@ -1,15 +1,20 @@
+// import { useAnecdoteActions } from '../store'
 import { useAnecdoteActions } from '../store'
+import anecdoteService from '../services'
 
 const AnecdoteForm = () => {
+  // const { addAnecdote } = useAnecdoteActions()
   const { addAnecdote } = useAnecdoteActions()
 
   return (
     <div>
       <h2>create new</h2>
-      <form onSubmit={(e) => {
+      <form onSubmit={ async (e) => {
         e.preventDefault()
-        addAnecdote(e.target.anecdote.value)
-        e.target.anecdote.value = ''
+        const newAnecdote = await anecdoteService.createNew(e.target.anecdote.value)
+        addAnecdote(newAnecdote)
+        e.target.reset()
+        // e.target.anecdote.value = ''
       }}>
         <div>
           <input name="anecdote" />
